8f6ca059746496441dd1195da925aa4632c9d9a6
Eddie Xie <oeddyo@gmail.com>
2020-06-14 20:40:34 -0700

    Add pip setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'kalasearch',         
+  packages = ['kalasearch'],   
+  version = '0.0.1',      
+  license='MIT',        
+  description = '卡拉搜索SDK-五分钟帮你打造站内、app内搜索引擎，适用于APP、小程序、电商、软件服务等领域',   
+  author = 'Eddie Xie',                 
+  author_email = 'oeddyo@gmail.com',    
+  url = 'https://kalasearch.cn',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['搜索', '后端', '中台', 'search', 'full-text search', '搜索引擎', '站内搜索'],   
+  install_requires=[            
+          'requests',
+      ],
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+  ],
+  include_package_data=True,
+  python_requires=">=3"
+)
