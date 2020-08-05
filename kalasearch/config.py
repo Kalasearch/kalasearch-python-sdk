608691df500c54b49b2a6eafa42949f5e54400be
Eddie Xie <oeddyo@gmail.com>
2020-08-04 23:00:09 -0700

    WIP

--- kalasearch/config.py
+++ kalasearch/config.py
@@ -2,7 +2,7 @@
 class Config():
     appId = None
     apiKey = None
-    domain = "https://api.kalasearch.cn"
+    domain = "https://api.kalasearch.cn/v1"
 
     def __init__(self, appId, apiKey):
         self.appId = appId
