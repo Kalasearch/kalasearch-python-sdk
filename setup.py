b192ea0986c958e74432ec8e11fb9446dcfcc5d4
Eddie Xie <oeddyo@gmail.com>
2020-08-04 23:04:12 -0700

    Update to 1.10.0

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open("README.md", "r") as fh:
 setup(
   name = 'kalasearch',         
   packages = ['kalasearch'],   
-  version = '1.0.1',      
+  version = '1.10.0',      
   license='MIT',        
   description = '卡拉搜索SDK-五分钟帮你打造站内、app内搜索引擎，适用于APP、小程序、电商、软件服务等领域',   
   author = 'Eddie Xie',                 
   author_email = 'oeddyo@gmail.com',    
   url = 'https://kalasearch.cn',
-  download_url = 'https://github.com/oeddyo/kalasearch-python-sdk/archive/1.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/oeddyo/kalasearch-python-sdk/archive/1.10.0.tar.gz',    # I explain this later on
   keywords = ['搜索', '后端', '中台', 'search', 'full-text search', '搜索引擎', '站内搜索'],   
   install_requires=[            
           'requests',
